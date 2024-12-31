9b24437604eb76e4a8ecf45009262ca77afb20c0
Luigi11162 <luigibacco01@gmail.com>
2024-12-31 19:06:00 +0100

    Implemented classes for Voter and Proposer

--- Proposer.py
+++ Proposer.py
@@ -1,31 +1,60 @@
-counter = 1
-my_propose = None
-i = None
+class Proposer:
+    def __init__(self, i, my_propose):
+        self.i = i
+        self.my_propose = my_propose
+        self.counter = 1
+
+    def init_round(self, majority, time_max):
+        self.counter += 1
+        wait_voters = 0
+        last = []
+        r = (self.counter, self.i)
+        send_propose(r)
+        time = 0
+        while wait_voters <= majority:
+            last.append(receive_proposes())
+            counter = max(last)
+            wait_voters += 1
+            if time <= time_max:
+                break
+        v = max(last)
+        if not v:
+            v = self.my_propose
+        send_begin(r, v)
+        wait_voters = 0
+        while wait_voters <= majority:
+            receive_votes(r)
+            wait_voters += 1
+            if time <= time_max:
+                break
+        choose(v, r)
+        send_success(v)
+        receive_ack()
 
 
-def init_round():
-    global counter
-    counter += 1
-    wait_voters = 0
-    last = []
-    r = (counter, i)
-    send_propose(r)
-    while wait_voters <= majority:
-        last.append(receive_proposes(r, last, new_r, v))
-        counter = max_counter
-        wait_voters += 1
-        if time <= time_max:
-            break
-    v = max(last)
-    if not v:
-        v = my_propose
-    send_begin(r, v)
-    wait_voters = 0
-    while wait_voters <= majority:
-        receive_votes(r)
-        wait_voters += 1
-        if time <= time_max:
-            break
-    choose(v, r)
-    send_success(v)
-    receive_ack()
+def send_begin(r, v):
+    message = (r, v, "Begin")
+
+
+def send_propose(r):
+    message = (r, "Propose")
+
+
+def send_success(v):
+    message = (v, "Success")
+
+
+def receive_proposes():
+    return
+
+
+def receive_ack():
+    return
+
+
+def receive_votes(r):
+    return
+
+
+def choose(v, r):
+    return
--- Voter.py
+++ Voter.py
@@ -1,23 +1,40 @@
-i = None
-last_r = (0, i)
-last_v = input_i
-commit = (0, i)
+class Voter:
+    def __init__(self, i, last_v):
+        self.i = i
+        self.last_r = (0, i)
+        self.last_v = last_v
+        self.commit = (0, i)
+
+    def vote(self, message):
+        r = message["r"]
+        if message["type"] == "Collect":
+            if r >= self.commit:
+                send_last(r, self.last_r, self.last_v)
+                self.commit = r
+            else:
+                send_old_round(r, self.commit)
+
+        if message["type"] == "Begin":
+            v = message["v"]
+            if r >= self.commit:
+                send_accept(r)
+                self.last_r = r
+                self.last_v = v
+            else:
+                send_old_round(r, self.commit)
 
 
-def vote(message):
-    r = message["r"]
-    if message["type"] == "Collect":
-        if r >= commit:
-            send(r, last, last_r, last_v)
-            commit = r
-        else:
-            send(r, old_round, commit)
+def send(content, round, type=""):
+    #TODO implement send function
+    pass
 
-    if message["type"] == "Begin":
-        v= message["v"]
-        if r >= commit:
-            send_accept(r)
-            last_r = r
-            last_v = v
-        else:
-            send_old_round(r, commit)
+
+def send_accept(r):
+    message = (r, "Accept")
+
+
+def send_old_round(r, commit):
+    message = (r, "Old Round", commit)
+
+def send_last(r, last_r, last_v):
+    message = (r, "Last Round", last_r, last_v)
